005d78e93493fad133c27d306b66f19043742297
ze0nni <ze0nni@gmail.com>
2020-02-12 02:45:10 +0300

    fix: 'TestLibrary_Name'

--- internal/engine/Library_test.go
+++ internal/engine/Library_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestLibrary_Name(t *testing.T) {
-	l := newLibraryInst("foo", nil, nil, nil)
+	l := newLibraryInst("foo", LensOf("schema", driver.InMemory()), nil, nil)
 
 	assert.Equal(t, "foo", l.Name())
 }
