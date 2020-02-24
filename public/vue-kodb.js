8a211ecfb04f96d4e2de6b776345a437a18e80d6
ze0nni <ze0nni@gmail.com>
2020-02-24 12:39:43 +0300

    Change layouts for application

--- public/vue-kodb.js
+++ public/vue-kodb.js
@@ -21,27 +21,56 @@ Vue.component("kodb", {
         template:
 `
 <v-app>
-        <v-content>
-                <v-tabs
-                        v-model="selectedLibrary"
-                        show-arrows
-                >
-                        <v-tab
-                                v-for="l in librarys"
-                                :key="l.id"
-                        >
-                                {{ l.name }}
-                        </v-tab>
+        <v-app-bar
+                app
+                clipped-left
+                dense  
+        >
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-                        <v-tab-item
-                                v-for="l in librarys"
-                                :key="l.id"
+                <v-toolbar-title>Page title</v-toolbar-title>
+
+                <v-spacer></v-spacer>
+
+                <v-btn icon>
+                        <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                        <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+        
+
+                <template v-slot:extension>
+                        <v-tabs
+                                v-model="selectedLibrary"
+                                show-arrows
                         >
-                                <kodb-library
-                                        v-bind:librarySchema="l"
-                                />
-                        </v-tab-item>
-                </v-tabs>
+                                <v-tab
+                                        v-for="l in librarys"
+                                        :key="l.id"
+                                >
+                                        {{ l.name }}
+                                </v-tab>
+                        </v-tabs>
+                </template>
+        </v-app-bar>
+
+        <v-content>
+                <v-container>
+                        <v-tabs-items
+                                v-model="selectedLibrary"
+                        >
+                                <v-tab-item
+                                        v-for="l in librarys"
+                                        :key="l.id"
+                                >
+                                        <kodb-library
+                                                v-bind:librarySchema="l"
+                                        />
+                                </v-tab-item>
+                        </v-tabs-items>
+                </v-container>
         </v-content>
 </v-app>
 `
