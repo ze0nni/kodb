f60f4e7bb89889a9cb4f066914d0acd41c182d58
ze0nni <ze0nni@gmail.com>
2020-02-24 09:20:02 +0300

    Add 'Row.Get' and 'Row.Set'

--- internal/engine/Row.go
+++ internal/engine/Row.go
@@ -1,5 +1,9 @@
 package engine
 
+import (
+	"errors"
+)
+
 type (
 	// RowID type
 	RowID string
@@ -8,9 +12,9 @@ type (
 	Row interface {
 		Exists() (bool, error)
 
-		// Get(ColumnID) (string, bool, error)
-		// Has(ColumnID) bool
-		// Set(ColumnID, string) error
+		Get(ColumnID) (string, bool, error)
+		// Has(ColumnID) (bool, error)
+		Set(ColumnID, string) error
 		// Delete(ColumnID) error
 
 		private()
@@ -42,6 +46,33 @@ func (r *rowImpl) Exists() (bool, error) {
 		return false, err
 	}
 	return nil != e, nil
+}
+
+func (r *rowImpl) Get(id ColumnID) (string, bool, error) {
+	e, err := r.data.Get(r.id.ToString())
+	if nil != err {
+		return "", false, err
+	}
+	if nil == e {
+		return "", false, errors.New("Row not exists:" + r.id.ToString())
+	}
+	v, ok := e[id.ToString()]
+	if false == ok {
+		return "", false, nil
+	}
+	return v, true, nil
+}
+
+func (r *rowImpl) Set(id ColumnID, value string) error {
+	e, err := r.data.Get(r.id.ToString())
+	if nil != err {
+		return err
+	}
+	if nil == e {
+		return errors.New("Row not exists:" + r.id.ToString())
+	}
+	e[id.ToString()] = value
+	return r.data.Put(r.id.ToString(), e)
 }
 
 func (r *rowImpl) private() {
--- internal/engine/Row_test.go
+++ internal/engine/Row_test.go
@@ -38,3 +38,81 @@ func TestRow_Exists(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, e)
 }
+
+func TestRow_Get_Error_when_row_not_exists(t *testing.T) {
+	r := RowOf(
+		LensOf("x", driver.InMemory()),
+		RowID("foo"),
+	)
+	_, _, err := r.Get(ColumnID("key"))
+
+	assert.Error(t, err)
+}
+
+func TestRow_Get_row_not_found(t *testing.T) {
+	l := LensOf("x", driver.InMemory())
+	id := RowID("foo")
+
+	l.Put(id.ToString(), make(entry.Entry))
+
+	r := RowOf(
+		l,
+		id,
+	)
+
+	_, ok, err := r.Get(ColumnID("key"))
+
+	assert.False(t, ok)
+	assert.NoError(t, err)
+}
+
+func TestRow_Get(t *testing.T) {
+	l := LensOf("x", driver.InMemory())
+	id := RowID("foo")
+	key := ColumnID("key")
+
+	e := make(entry.Entry)
+	e[key.ToString()] = "value"
+	l.Put(id.ToString(), e)
+
+	r := RowOf(
+		l,
+		id,
+	)
+
+	v, ok, err := r.Get(key)
+
+	assert.Equal(t, "value", v)
+	assert.True(t, ok)
+	assert.NoError(t, err)
+}
+
+func TestRow_Set_error_when_row_not_exists(t *testing.T) {
+	r := RowOf(
+		LensOf("x", driver.InMemory()),
+		RowID("foo"),
+	)
+
+	err := r.Set(ColumnID("key"), "value")
+
+	assert.Error(t, err)
+}
+
+func TestRow_Set(t *testing.T) {
+	l := LensOf("x", driver.InMemory())
+	id := RowID("foo")
+	l.Put(id.ToString(), make(entry.Entry))
+
+	r := RowOf(
+		l,
+		id,
+	)
+
+	key := ColumnID("key")
+	err := r.Set(key, "value")
+	v, ok, _ := r.Get(key)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "value", v)
+	assert.True(t, ok)
+}
