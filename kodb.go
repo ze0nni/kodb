eec6cca2588b4db4a93e778c8790e270fbdc5e05
e.blagodarev <ze0nni@gmail.com>
2020-02-28 18:24:31 +0300

    Generate random data with 'github.com/Pallinder/go-randomdata'

--- kodb.go
+++ kodb.go
@@ -7,29 +7,32 @@ import (
 	"github.com/ze0nni/kodb/internal/driver"
 	"github.com/ze0nni/kodb/internal/engine"
 	"github.com/ze0nni/kodb/run/web"
+
+	"github.com/Pallinder/go-randomdata"
 )
 
 func main() {
 	eng := engine.New(driver.InMemory())
 	userLib := eng.GetLibrary(engine.LibraryName("user"))
-	firstname, _ := userLib.NewColumn("firstname")
+	firstname, _ := userLib.NewColumn("firstName")
 	secondName, _ := userLib.NewColumn("secondName")
 	age, _ := userLib.NewColumn("age")
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 		userLib.NewRow()
 		row, _ := userLib.Row(i)
-		row.Set(firstname, "First name")
-		row.Set(secondName, "Second name")
-		row.Set(age, strconv.Itoa(i*5))
+		row.Set(firstname, randomdata.FirstName(0))
+		row.Set(secondName, randomdata.LastName())
+		row.Set(age, strconv.Itoa(randomdata.Number(16, 40)))
 	}
 
-	invLib := eng.GetLibrary(engine.LibraryName("inventory"))
-	invLib.NewColumn("name")
+	invLib := eng.GetLibrary(engine.LibraryName("location"))
+	name, _ := invLib.NewColumn("name")
 	invLib.NewColumn("type")
 	invLib.NewColumn("title")
 	invLib.NewColumn("picture")
 	for i := 0; i < 20; i++ {
-		invLib.NewRow()
+		row, _ := invLib.NewRow()
+		invLib.UpdateValue(row, name, randomdata.Adjective())
 	}
 
 	err := web.Run(eng)
